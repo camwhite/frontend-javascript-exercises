229577b45186434577382d4ccfcc361053334549
Cameron White <camw85@mac.com>
2014-10-06 19:00:57 +0000

    Arrays down

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,9 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(index0, index1, index2, index3) {
+  return [index0, index1, index2, index3];
+};
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(array) {
+  first = array[0];
+  last = array[array.length - 1];
+  return [first, last];
+};
