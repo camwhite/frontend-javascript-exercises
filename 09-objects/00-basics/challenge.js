6b6944cb27b9057876e6506f7d4b7b0667eb9160
Cameron White <camw85@mac.com>
2014-10-06 21:55:34 +0000

    Object exercises done

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,29 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(prop1,prop2, prop3) {
+   var obj = {
+    title: prop1,
+    duration: prop2,
+    students: prop3
+  }
+  return obj
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(obj, newProp, newVal) {
+  if(! obj.hasOwnProperty(newProp)) {
+    obj[newProp] = newVal;
+  }
+  return obj;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter) {
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;
+};
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item, money) {
+  obj = {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+  }
+  return money >= obj[item]
+};
