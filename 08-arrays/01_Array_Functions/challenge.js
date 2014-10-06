d7d780f2160acc5b5bcdb02bb730b5ac483eb0d4
Cameron White <camw85@mac.com>
2014-10-06 20:07:50 +0000

    Arrays01 down

--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,13 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(array) {
+  flipped = array.reverse();
+  flipped.unshift(1);
+  return flipped;
+};
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(array) {
+  return array.join('+');
+};
 
-module.exports.arrayQuantityPlusOne = undefined;
+module.exports.arrayQuantityPlusOne = function(array) {
+  return array.length + 1;
+};
