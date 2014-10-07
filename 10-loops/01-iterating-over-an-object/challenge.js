3412f869e42e1207b8bf4f7a939377f741fab73a
Cameron White <camw85@mac.com>
2014-10-07 00:53:16 +0000

    Looped the loop

--- 10-loops/01-iterating-over-an-object/challenge.js
+++ 10-loops/01-iterating-over-an-object/challenge.js
@@ -1,5 +1,22 @@
-module.exports.getKeys = undefined;
+module.exports.getKeys = function(obj) {
+    var array = [];
+    for(var key in obj) {
+      array.push(key);
+    }
+    return array;
+};
 
-module.exports.getValues = undefined;
+module.exports.getValues = function(obj) {
+  var array = [];
+  for(var prop in obj) {
+    array.push(obj[prop]);
+  }
+  return array;
+};
 
-module.exports.objectToArray = undefined;
+module.exports.objectToArray = function(obj) {
+  var keys = Object.keys(obj);
+  return keys.map(function(key) {
+    return key + " is " + obj[key];
+  })
+};
