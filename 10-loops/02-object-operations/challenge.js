ce84e2464e8c15ed5a404df554b924a395f4281b
Cameron White <camw85@mac.com>
2014-10-07 01:52:38 +0000

    I like loops

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,24 @@
-module.exports.copy = undefined;
+module.exports.copy = function(obj) {
+  newObj = {};
+  for(var prop in obj) {
+    newObj[prop] = obj[prop];
+  }
+  return newObj;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest, src) {
+  for(var prop in src) {
+    dest[prop] = src[prop];
+  }
+  return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(obj, array) {
+  var returnVar = true;
+  array.forEach(function(key) {
+    if(! obj.hasOwnProperty(key)) {
+      returnVar = false;
+    }
+  });
+  return returnVar;
+};
