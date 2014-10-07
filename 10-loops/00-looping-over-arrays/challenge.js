d9ff6863a26f4d17fbf46f269044b60b40fcf222
Cam White <camw85@mac.com>
2014-10-06 20:03:24 -0600

    Looped over arrays

--- 10-loops/00-looping-over-arrays/challenge.js
+++ 10-loops/00-looping-over-arrays/challenge.js
@@ -1,5 +1,17 @@
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function(array) {
+  returnVar = 0;
+  for(var i = 0; i < array.length; i++) {
+    returnVar += array[i];
+  }
+  return returnVar;
+};
 
-module.exports.splitAndLowerCaseString = undefined;
+module.exports.splitAndLowerCaseString = function(inputString) {
+  return inputString.toLowerCase().split(',')
+};
 
-module.exports.addIndex = undefined;
+module.exports.addIndex = function(array) {
+  return array.map(function(element, index) {
+    return index + ' is ' + element; 
+  });
+};
